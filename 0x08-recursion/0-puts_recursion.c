219eec077318f7ae2e46677de8f9536f6b2f845a
sunbase-ai <farouk0021@gmail.com>
2023-06-12 15:04:45 +0100

    0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,11 +10,5 @@ void _puts_recursion(char *s){
     if (*s) {putchar(*s);
     _puts_recursion(s+1);
     }
-    else {putchar('\n');}
-}
-
-int main(void)
-{
-    _puts_recursion("Puts with recursion");
-    return (0);
+    else {_putchar('\n');}
 }
