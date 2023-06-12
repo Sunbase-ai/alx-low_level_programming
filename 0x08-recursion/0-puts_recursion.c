3426b1923b10d9f8cb617cd86a8d71df32a96a82
sunbase-ai <farouk0021@gmail.com>
2023-06-12 15:10:38 +0100

    0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,14 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
 //----------------------------------------------------------------function for printing----------------------------------------------------------------
 
 
 
-void _puts_recursion(char *s){
-    if (*s) {_putchar(*s);
-    _puts_recursion(s+1);
+void _puts_recursion(char *s)
+{
+    if (*s)
+    {
+        _putchar(*s);
+        _puts_recursion(s+1);
     }
-    else {_putchar('\n');}
+    else
+    {
+        _putchar('\n');
+    }
 }
