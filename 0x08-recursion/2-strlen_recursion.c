d89471c7011de089873c920ace155e62abeda607
sunbase-ai <farouk0021@gmail.com>
2023-06-14 12:10:53 +0100

    0x08-recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int _strlen_recursion(char *s){
+
+}
+
+int main(void)
+{
+    int n;
+    
+    n = _strlen_recursion("Corbin Coleman");
+    printf("%d\n", n);
+    return (0);
+}
